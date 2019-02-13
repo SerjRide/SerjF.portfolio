19999385a187edfdbbd639996f7b9100a7c6a164
SerjRide <quicksquicks6@gmail.com>
2019-02-13 13:07:34 +0300

    added index.js and nav.js

---
+++ index.js
@@ -0,0 +1,40 @@
+$("#modal-1").on("click",() => {
+  $('.modalName').text(`STYLISH HOME white decision`);
+  $('.modalText')
+    .text(`Пример одностраничного сайта, выполненного в ярких, серых тонах. Компания STYLISH HOME, представленная на странице, является вымышленной. Все совпадения с реально существующими компаниями - случайны. Весь текст на странице представлен в качестве примера и не несет в себе никакого информационного посыла.`);
+  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS Grid, CSS transforms, Animate.css ');
+  $('.modalImg')
+    .html('<img class="imgUnderModel" src="../../img/portfolio/p1.jpg">')
+  $('.modalLink')
+    .attr('href','./components/site 1')
+});
+$("#modal-2").on("click",() => {
+  $('.modalName').text(`SOUNDPLACE black decision`);
+  $('.modalText')
+    .text(`Пример одностраничного сайта, выполненного в тёмных тонах. Компания SOUNDPLACE, представленная на странице, является вымышленной. Все совпадения с реально существующими компаниями - случайны. Весь текст на странице представлен в качестве примера и не несет в себе никакого информационного посыла.`);
+  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS Grid, CSS transforms, Animate.css ');
+  $('.modalImg')
+    .html('<img class="imgUnderModel" src="../../img/portfolio/p2.jpg">')
+  $('.modalLink')
+    .attr('href','./components/site 2')
+});
+$("#modal-3").on("click",() => {
+  $('.modalName').text(`STYLISH HOME dark decision`);
+  $('.modalText')
+    .text(`Пример одностраничного сайта, выполненного в тёмных, серых тонах. Компания STYLISH HOME, представленная на странице, является вымышленной. Все совпадения с реально существующими компаниями - случайны. Весь текст на странице представлен в качестве примера и не несет в себе никакого информационного посыла.`);
+  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS Grid, CSS transforms, Animate.css ');
+  $('.modalImg')
+    .html('<img class="imgUnderModel" src="../../img/portfolio/p3.jpg">')
+  $('.modalLink')
+    .attr('href','./components/site 3')
+});
+$("#modal-4").on("click",() => {
+  $('.modalName').text(`STYLISH HOME colorful solution`);
+  $('.modalText')
+    .text(`Пример одностраничного сайта, выполненного в красочных, ярко-оранжевых тонах. Компания STYLISH HOME, представленная на странице, является вымышленной. Все совпадения с реально существующими компаниями - случайны. Весь текст на странице представлен в качестве примера и не несет в себе никакого информационного посыла.`);
+  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS Grid, CSS transforms, Animate.css ');
+  $('.modalImg')
+    .html('<img class="imgUnderModel" src="../../img/portfolio/p4.jpg">')
+  $('.modalLink')
+    .attr('href','./components/site 4')
+});
---
+++ nav.js
@@ -0,0 +1,54 @@
+const link = 'serjride.local'
+$('#import-nav').html(
+  `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">
+      <img width="25"
+        src="./img/logo_black.png"
+        alt="logo"/>
+    </a>
+    <button class="navbar-toggler"
+      type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto "></ul>
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">HOME</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"
+            href="#" id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false">
+            WORKS
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item"
+              href="http://${link}/components/site%201/">
+              SH white decision
+            </a>
+            <a class="dropdown-item"
+              href="http://${link}/components/site%202/">
+              SP black decision
+            </a>
+            <a class="dropdown-item"
+              href="http://${link}/components/site%203/">
+              SH dark decision
+            </a>
+            <a class="dropdown-item"
+              href="http://${link}/components/site%204/">
+              SH colorful solution
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>`
+);
