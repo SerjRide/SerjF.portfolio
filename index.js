9d71de1b63c143a5e587760900a25fbf9b7e4672
SerjRide <quicksquicks6@gmail.com>
2019-02-18 13:14:38 +0300

    fixed discription for modal windows

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ $("#modal-4").on("click",() => {
 $("#modal-5").on("click",() => {
   $('.modalName').text(`Star Claster`);
   $('.modalText')
-    .text(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS animation');
+    .text(`Пример одностраничного сайта с использованием JS и jQuery. Переход между страницами сайта осуществляется при помощи изменения содержимого блока <content> без перезагрузки страницы. Так же на сайте происходит рандомная генерация декоративных объектов (звёзд) при помощи JS и css.animation. Страница адаптирована под различные устройства и размеры экрана.`);
+  $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, Bootstrap, Flexbox, CSS animation, JavaScript, jQuery');
   $('.modalImg')
     .html('<img class="imgUnderModel" src="../../img/portfolio/p5.jpg">')
   $('.modalLink')
@@ -51,7 +51,7 @@ $("#modal-5").on("click",() => {
 $("#modal-6").on("click",() => {
   $('.modalName').text(`Fractal Board`);
   $('.modalText')
-    .text(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+    .text(`Пример одностраничного сайта с использованием JS и jQuery. Переход между страницами сайта осуществляется при помощи изменения содержимого блока <content> без перезагрузки страницы. Построение диаграмм осуществляется при помощи библиотеки Morris.js. Генерация и работа с таблицей осуществляется при помощи библиотеки MDB Bootstrap. Страница адаптирована под различные устройства и размеры экрана.`);
   $('.spanText').html('<span>Используемые технологии: </span> HTML5, CSS3, MDB Bootstrap, SVG, Morris.js');
   $('.modalImg')
     .html(`<img class="imgUnderModel" src="../../img/portfolio/p6.jpg">
